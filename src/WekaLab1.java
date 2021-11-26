c3d72bec7ae404b32c93bdbde6c0877d31ac239f
valerygretchishnikov <gretcishnikov@mail.ru>
2021-11-26 17:01:37 +0300

    Init commit

--- src/WekaLab1.java
+++ src/WekaLab1.java
@@ -6,27 +6,28 @@ import weka.core.Instances;
 import weka.classifiers.trees.J48;
 
 public class WekaLab1 {
-    public static void main (String []args){
-        try{
-            BufferedReader reader = new BufferedReader(new FileReader("D:/Weka-3-6/data/bmw-training.arff"));
+    public static void main (String []args) throws IOException {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("data/bmw-training.arff"));
             Instances train = new Instances (reader);
-            train.setClassIndex(train.numAttributes()-1);
-            BufferedReader reader2 = new BufferedReader(new FileReader("D:/Weka-3-6/data/bmw-testing.arff"));
+            train.setClassIndex(train.numAttributes() - 1);
+            BufferedReader reader2 = new BufferedReader(new FileReader("data/bmw-testing.arff"));
             Instances test = new Instances (reader2);
-            test.setClassIndex(test.numAttributes()-1);
-            if(!train.equaHeaders(test))
-            throw new IllegalArgumentException("Train and test set are not compatible!");
+            test.setClassIndex(test.numAttributes() - 1);
+            if(!train.equalHeaders(test))
+                throw new IllegalArgumentException("Train and test set are not compatible!");
             J48 cls = new J48();
-            cls.buildClassificator(train);
+            cls.buildClassifier(train);
             System.out.println("#-actual-predicted-error-distribution");
-            for (int i = 0; i < test.numInstances(); i++) {
-                double pred = cls.classifyInstance(test.instance(i));
+            for (int i = 0; i < train.numInstances(); i++) {
+                double pred = cls.classifyInstance(train.instance(i));
                 System.out.println("->");
-                System.out.println(test.classAttribute().value(int) pred));
+                System.out.println(train.classAttribute().value((int)pred));
                 System.out.println("\n Weka ended");
             }
-            catch (Exception e){
-                System.out.println("Weka Error" + e.getMessage());
+        }
+        catch (Exception e){
+            System.out.println("Weka Error" + e.getMessage());
         }
     }
 }
